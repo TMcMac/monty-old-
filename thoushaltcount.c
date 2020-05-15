4993707545d3c8518fe66b10974d0833af030c52
timmcmackenjr@gmail.com <timmcmackenjr@gmail.com>
2020-05-15 23:09:20 +0000

    Cleaned up all the math functions, they now use pop. Refactored _swap

--- thoushaltcount.c
+++ thoushaltcount.c
@@ -13,10 +13,7 @@ void _add(stack_t **stack, unsigned int line_number __attribute__ ((unused)))
 
 	result = ((*stack)->n + mover->n);
 	mover->n = result;
-	mover = mover->next;
-	(*stack) = (*stack)->prev;
-	free(mover);
-
+	_pop(stack, line_number);
 }
 
 /**
@@ -33,9 +30,7 @@ void _sub(stack_t **stack, unsigned int line_number)
 
 	result = ((*stack)->n - mover->n);
 	mover->n = result;
-	mover = mover->next;
-	(*stack) = (*stack)->prev;
-	free(mover);
+	_pop(stack, line_number);
 }
 
 /**
@@ -52,10 +47,7 @@ void _div(stack_t **stack, unsigned int line_number)
 
 	result = (mover->n / (*stack)->n);
 	mover->n = result;
-	mover = mover->next;
-	(*stack) = (*stack)->prev;
-	free(mover);
-
+	_pop(stack, line_number);
 }
 
 /**
@@ -72,10 +64,7 @@ void _mul(stack_t **stack, unsigned int line_number)
 
 	result = ((*stack)->n * mover->n);
 	mover->n = result;
-	mover = mover->next;
-	(*stack) = (*stack)->prev;
-	free(mover);
-
+	_pop(stack, line_number);
 }
 
 /**
@@ -92,8 +81,5 @@ void _mod(stack_t **stack, unsigned int line_number)
 
 	result = (mover->n % (*stack)->n);
 	mover->n = result;
-	mover = mover->next;
-	(*stack) = (*stack)->prev;
-	free(mover);
-
+	_pop(stack, line_number);
 }
