4b009676118ed61f0baad1b5f105d0c61d5b3239
StaciAF <aaen.it19@gmail.com>
2020-05-15 02:42:51 +0000

    removed %d from line_number calls

--- basic.c
+++ basic.c
@@ -63,7 +63,7 @@ void _pint(stack_t **stack, unsigned int line_number)
 {
 	if ((*stack) == NULL || stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	else
